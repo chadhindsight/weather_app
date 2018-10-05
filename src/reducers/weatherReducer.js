7c95a0856c520bf9394912cef77208ba6d4436cd
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2018-10-05 12:39:02 -0400

    return default state in default case

--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -1,12 +1,13 @@
-import {FETCH_WEATHER} from  './actions/actionsIndex';
+import {FETCH_WEATHER} from  '../actions/actionIndex';
 
 const weatherReducer = (state =[], action) => {
 	switch(action.type) {
 		case FETCH_WEATHER:
 		// return data from requested url
 			return [action.payload.data, ...state];
+		default:
+		return state;
 	}
-	return state
 }
 
 export default weatherReducer;
